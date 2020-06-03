70e9d6d746039c428ba48c58fdf44e3f86fd2c46
dmitry <dmitry.polushkin@gmail.com>
2020-06-03 20:04:37 +0200

    Root should work as well, add next to the beforeEach

--- src/boot/preload.js
+++ src/boot/preload.js
@@ -2,6 +2,8 @@ export default async ({ store, router, ssrContext, urlPath }) => {
   router.beforeEach(async (to, from, next) => {
     if (to.name === 'auth') {
       next({ name: 'home' })
+    } else {
+      next()
     }
   })
 }
